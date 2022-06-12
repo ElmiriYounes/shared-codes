e5d7cd537c3d6d19eabf9bc35fcea3269e41462e
ElmiriYounes <elmiri.younes@hotmail.com>
2022-06-12 18:54:31 +0200

    r

--- drag-drop/script.js
+++ drag-drop/script.js
@@ -3,7 +3,7 @@ let target = document.getElementById("target");
 let images = document.querySelectorAll(".image");
 
 // disable drag/drop for image target
-display.querySelector('img').draggable = false;
+display.querySelector("img").draggable = false;
 
 /**
  * Drag/Drop for desktop with mouse cursor
@@ -18,7 +18,7 @@ display.querySelector('img').draggable = false;
       event.dataTransfer.setData("sourceUrl", image.getAttribute("src"));
     });
     // dragend = end of drag
-    image.addEventListener("dragend", () => {
+    image.addEventListener("dragend", (event) => {
       // opacity 1 on dragend
       image.style.opacity = "1";
     });
